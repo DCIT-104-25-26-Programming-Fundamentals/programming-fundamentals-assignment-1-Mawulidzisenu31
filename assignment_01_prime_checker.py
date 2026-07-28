ac9f14ba67ec9f2374ff39dddd937f1b84b55d1c
Mawulidzisenu31 <mawulidzisenu31@gmail.com>
2026-07-28 18:25:18 +0000

    Add prime number checker function
    Implement prime number checking function.

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -34,4 +34,16 @@
 # =============================================================================
 # YOUR CODE BELOW — remove the # symbols from the scaffold and fill it in
 # =============================================================================
-
+def prime_no(n):
+  if n<2:
+    return False
+  for i in range(2,n):
+    if n%i ==0:
+      return False
+    return True
+if __name__=="__main__":
+  number= int(input("Enter a number: "))
+if prime_no(number):
+  print(f"{number} is a prime number.")
+else:
+  print(f"{number} is NOT a prime number")
